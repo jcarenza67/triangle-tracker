c2690c9d37664ac23aa19bac2c3b2777905f4cc0
Joseph Wilfong <josephwilfong91@gmail.com>
2023-03-23 16:23:50 -0700

    add labels, inputs, button, parameters, functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
+//Business logic
+
+
+
 //User Interface
-window.addEventListener("load", function(){
-  const form = document.getElementById("triangle");
-  form.addEventListener("submit", handleCalculation);
-})
+window.addEventListener("load", function () {
+    const form = document.getElementById("triangle");
+    form.onsubmit = function(event) {
+    let EqDorito = document.getElementById ("Equilateral");
+    EqDorito.setAttribute("class", "hidden");
+    let IsoPizza = document.getElementById ("Isosceles");
+    IsoPizza.setAttribute("class", "hidden");
+    let ScaleneTruck = document.getElementById ("Scalene");
+    ScaleneTruck.setAttribute("class", "hidden");
+    }
+  });
+  
+  function displayPicture(event){
+    const input1 = parseFloat(document.querySelector("input#input1").value);
+    const input2 = parseFloat(document.querySelector("input#input2").value);
+    const input3 = parseInt(document.querySelector("input#input3").value);
+    event.preventDefault();
+    
+
+    document.querySelector("div#Equilateral").removeAttribute("class");
+    document.querySelector("div#Isosceles").removeAttribute("class");
+    document.querySelector("div#Scalene").removeAttribute("class");
+
+  }
+  let result;
+  if (input1 === input2 && input1 === input3){
+    result = EqDorito.removeAttribute("class")
+  }
+  
