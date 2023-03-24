d1a8fe2cdf27138c56bac660824ee2213864c4a4
Joseph Wilfong <josephwilfong91@gmail.com>
2023-03-23 21:53:03 -0700

    Fix up Javascript to actually work

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,28 @@
 
 //User Interface
 window.addEventListener("load", function () {
-    
-  });
-  
-  function display(event){
-    const input1 = parseFloat(document.querySelector("input#input1").value);
-    const input2 = parseFloat(document.querySelector("input#input2").value);
-    const input3 = parseInt(document.querySelector("input#input3").value);
-    event.preventDefault();
-    
+  const form = document.getElementById("triangle");
+  form.addEventListener("submit", display);
+});
 
-  
-  }
+function display(event){
+  const input1 = parseFloat(document.querySelector("input#input1").value);
+  const input2 = parseFloat(document.querySelector("input#input2").value);
+  const input3 = parseFloat(document.querySelector("input#input3").value);
+  event.preventDefault();
+
   let result;
-  if (input1 === input2 && input1 === input3){
-    result = display
-    document.getElementById("output").innerText = result;
+  if (input1 === input2 && input2 === input3){
+    result = "All inputs are equal";
+  } else if (input1 === input2 && input1 !== input3){
+    result = "This is an isosceles triangle with two equal sides";
+  } else if (input1 === input3 && input1 !== input2){
+    result = "This is an isosceles triangle with two equal sides";
+  } else if (input2 === input3 && input2 !== input1){
+    result = "This is an isosceles triangle with two equal sides";
+  } else {
+    result = "This is a scalene triangle with no equal sides";
   }
-  
+  document.getElementById("output").innerText = result;
+}
+
